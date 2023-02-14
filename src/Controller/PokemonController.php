c85fb2ad4ee70977767ad78ae0e0cb71fc975079
XenoX <gauthier.burgeat@gmail.com>
2023-02-14 21:58:38 +0100

    step three

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Pokemon;
+use App\Form\PokemonType;
 use App\Repository\PokemonRepository;
-use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,18 +22,21 @@ class PokemonController extends AbstractController
     }
 
     #[Route('/create', name: 'app_pokemon_create')]
-    public function create(PokemonRepository $pokemonRepository, TypeRepository $typeRepository): Response
+    public function create(Request $request, PokemonRepository $pokemonRepository): Response
     {
-        $pokemonRepository->save(
-            (new Pokemon())
-                ->setName('Raichu')
-                ->setNumber(26)
-                ->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png')
-                ->addType($typeRepository->findOneBy(['name' => 'Electric'])),
-            true,
-        );
+        $pokemon = new Pokemon();
+        $form = $this->createForm(PokemonType::class, $pokemon);
 
-        return $this->redirectToRoute('app_pokemon_index');
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pokemonRepository->save($pokemon, true);
+
+            return $this->redirectToRoute('app_pokemon_index');
+        }
+
+        return $this->render('pokemon/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     #[Route('/{id}', name: 'app_pokemon_show')]
---
+++ src/Form/PokemonType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pokemon;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PokemonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('number', IntegerType::class)
+            ->add('image', TextType::class, [
+                'attr' => [
+                    'value' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+                ],
+            ])
+            ->add('types', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pokemon::class,
+        ]);
+    }
+}
